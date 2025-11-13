502b57c640c89adac05e75982169609319ffbfab
egh13 <ericgarciahendricks@gmail.com>
2025-11-13 20:16:32 +0100

    carousel added

--- ClashAPP/src/app/app.ts
+++ ClashAPP/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar.component/sidebar.component';
+import { HeaderComponent } from './header.component/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- ClashAPP/src/app/index.component/index.component.ts
+++ ClashAPP/src/app/index.component/index.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../carousel.component/carousel.component';
 
 @Component({
   selector: 'app-index.component',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
 export class IndexComponent {
-  
+
 }
