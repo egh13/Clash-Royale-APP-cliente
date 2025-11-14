bddcee0594f911229b75aa75c5de6c4e7d7739d2
Gaton <danielgatonvega@gmail.com>
2025-11-14 13:27:39 +0100

    cambio de menu para añadir el componente mazos

--- ClashAPP/src/app/app.routes.ts
+++ ClashAPP/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index.component/index.component';
+import { MazosComponent } from './mazos.component/mazos.component';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: IndexComponent,
   },
+  {
+    path: 'mazos',
+    title: 'Recomendar Mazos',
+    component: MazosComponent,
+  }
 
-];
+];  
---
+++ ClashAPP/src/app/mazos.component/mazos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mazos.component',
+  imports: [],
+  templateUrl: './mazos.component.html',
+  styleUrl: './mazos.component.css',
+})
+export class MazosComponent {
+
+}
