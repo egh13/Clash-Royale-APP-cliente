f5c3b79c144c79c83447dfe034ae70ae3e4ff74a
egh13 <ericgarciahendricks@gmail.com>
2025-11-18 18:59:59 +0100

    backend añadido

--- ClashAPP/src/app/services/leaderboard.service.ts
+++ ClashAPP/src/app/services/leaderboard.service.ts
@@ -7,13 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class LeaderboardService {
 
+  constructor(private http: HttpClient) {}
+
   //cambiar porque no es seguro hardcodear la API_KEY
-  private API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM3MWI0YjdlLTBiZjMtNDUyNy04ZWQ5LTI2NzY3OTZiNjgwMCIsImlhdCI6MTc2Mjk0NDQzMywic3ViIjoiZGV2ZWxvcGVyLzhjMGZmZTg2LWQ4NjUtYTBiMy03ZTU1LTgxYjc5N2E0NjA4NiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.5Qrn_w-dqoXb5c2ypesKyChGnloUZIZqVtQKt593W-oELGX29ObFRuiE4t5npZw0Xr-360XSM7NzWBujeoMNyA'
+  private API_KEY =
+    '';
   private apiUrl = 'https://proxy.royaleapi.dev/v1/locations/global/seasons';
 
-  constructor(private http: HttpClient) {} 
-
-  obtenerDatos(){
-    return "hola";
+  obtenerLeaderboard(){
+    return this.http.get(this.apiUrl, {headers: {'Authorization': 'Bearer ' + this.API_KEY}});
   }
 }
