b7bb09ec82e17074a3841fa3988dbdc1a1d1aa54
egh13 <ericgarciahendricks@gmail.com>
2025-11-19 17:52:19 +0100

    leaderboards de clanes, creados e incluidos en index

--- ClashAPP/src/app/services/leaderboard.service.ts
+++ ClashAPP/src/app/services/leaderboard.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LeaderboardService {
-
   constructor(private http: HttpClient) {}
 
-  private apiUrl = "";
+  private apiUrl = 'http://localhost:3000/api/clans/rankings/'; // API endpoint
 
-  obtenerLeaderboard(){
-    return this.http.get(this.apiUrl);
+  obtenerLeaderboard(countryCode:string): Observable<any> {
+    return this.http.get(this.apiUrl + countryCode);
   }
 }
