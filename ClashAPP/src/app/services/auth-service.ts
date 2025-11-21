49eed3354a7b3063f009266f07e2d5c985a79dd5
egh <ericgarciahendricks@gmail.com>
2025-11-21 10:57:41 +0100

    authservice

--- ClashAPP/src/app/services/auth-service.ts
+++ ClashAPP/src/app/services/auth-service.ts
@@ -8,17 +8,10 @@ import { Observable } from 'rxjs';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://localhost:3000';
-
-  login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/api/usuarios/login`, { username, password }); //obtener token de sesion
-  }
-
-  logout(): void {
-    localStorage.removeItem('authToken'); // Eliminar el token del localStorage
-  }
-
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('authToken'); // Verificar si el token está presente en localStorage
+  login(username: string, password: string) {
+    return this.http.post<any>(
+      'http://localhost:3000/api/usuarios/login',
+      { username, password },
+    )
   }
 }
