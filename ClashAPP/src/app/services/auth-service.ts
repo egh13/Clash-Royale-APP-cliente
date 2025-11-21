1918ddb27aee0f7cb472ba37faf51677ba18f638
egh <ericgarciahendricks@gmail.com>
2025-11-21 10:25:09 +0100

    login arreglado

--- ClashAPP/src/app/services/auth-service.ts
+++ ClashAPP/src/app/services/auth-service.ts
@@ -7,10 +7,10 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  login(usuario: string, password: string) {
+  login(username: string, password: string) {
     return this.http.post<any>(
       'http://localhost:3000/api/usuarios/login',
-      { usuario, password },
+      { username, password },
       { withCredentials: true }
     );
     //.shareReplay() //almacena en cache la respuesta del observable, evita que se pueda mandar varias veces el POST
