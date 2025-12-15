8c34941ceaa9c09e50bac8ff35506710075c4b94
Gutah71 <sergio.webb.guti@gmail.com>
2025-12-15 16:58:50 +0100

    Footer con logo, assets y sticky footer

--- ClashAPP/src/app/app.ts
+++ ClashAPP/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer.component/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
