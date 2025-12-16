54a73299bf8503f73d5f313f5141b7c80d3d9d15
egh <ericgarciahendricks@gmail.com>
2025-12-16 10:11:53 +0100

    refactor card-catalog a angular+20

---
+++ ClashAPP/src/app/services/leaderboard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LeaderboardService {
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = 'http://localhost:3000/api/developerApi/clans/rankings/'; // API endpoint
+
+  obtenerLeaderboard(countryCode:string): Observable<any> {
+    return this.http.get(this.apiUrl + countryCode);
+  }
+}
