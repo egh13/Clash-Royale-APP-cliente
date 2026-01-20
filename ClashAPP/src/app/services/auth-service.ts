601c1e2aa0f377363e2c1311adb6936207fc5223
egh <ericgarciahendricks@gmail.com>
2026-01-20 10:10:04 +0100

    presentacion cambia dependel estado de loggedIn

--- ClashAPP/src/app/services/auth-service.ts
+++ ClashAPP/src/app/services/auth-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +9,18 @@ import { Observable } from 'rxjs';
 export class AuthService {
   constructor(private http: HttpClient) { }
 
+  private loggedIn$ = new BehaviorSubject<boolean>(false); // se crea con valor inicial false
+  public isLoggedIn$ = this.loggedIn$.asObservable(); // el $ significa que es un observable
+
   private baseUrl = "http://localhost:3000/api/usuarios";
 
   login(username: string, password: string) {
+    this.loggedIn$.next(true);
     return this.http.post<any>(`${this.baseUrl}/login`,{ username, password })
+  }
+
+  logout() {
+    this.loggedIn$.next(false);
   }
 
   register(username: string, password: string): Observable<any> {
