dd1912105a16c49dce7f45a3e86d230af38b940a
egh13 <ericgarciahendricks@gmail.com>
2026-02-01 19:42:26 +0100

    Update user frontend

--- ClashAPP/src/app/components/profile/profile.ts
+++ ClashAPP/src/app/components/profile/profile.ts
@@ -1,5 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+
+type JwtPayload = {
+  id: number;
+  username: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-profile',
   imports: [],
@@ -10,9 +19,37 @@ import { AuthService } from '../../services/auth-service';
 export class Profile implements OnInit {
 
   private authService = inject(AuthService);
-  private token: string | null = '';
+  private router = inject(Router);
+
+  user!: JwtPayload;
 
   ngOnInit(): void {
-    this.token = this.authService.getToken();
+    const token = this.authService.getToken();
+
+    if (!token) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.user = jwtDecode<JwtPayload>(token);
+  }
+
+  deleteUser() {
+
+    // TODO aquí iría la llamada al backend
+    const confirmDelete = confirm('¿Seguro que deseas borrar tu cuenta?');
+
+    if (confirmDelete) {
+      console.log('Usuario eliminado:', this.user.id);
+      this.authService.logout();
+      this.router.navigate(['/login']);
+    }
+  }
+
+  changePassword() {
+
+    // TODO
+    console.log('Cambiar contraseña para:', this.user.username);
+    this.router.navigate(['/change-password']);
   }
 }
