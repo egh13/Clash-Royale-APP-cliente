a6d7056ce9e918d9e250623e0cf6a024734d75d3
egh13 <ericgarciahendricks@gmail.com>
2026-02-02 19:15:48 +0100

    Add more inputs to register form

--- ClashAPP/src/app/components/contacto/contacto.ts
+++ ClashAPP/src/app/components/contacto/contacto.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ToastService } from '../../services/toast-service';
 
 @Component({
   selector: 'app-contacto',
@@ -10,6 +11,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 export class Contacto {
 
   private fb = inject(FormBuilder);
+  private toast = inject(ToastService);
 
   contactForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -37,6 +39,9 @@ export class Contacto {
       this.contactForm.markAllAsTouched();
       return;
     }
+
+    // toast informativo 
+    this.toast.success("Formulario Enviado Correctamente");
 
     // Reset opcional
     this.contactForm.reset({
