8e14f53c3500b3d1c038d562d591735af5ee6f8d
egh13 <ericgarciahendricks@gmail.com>
2026-02-10 20:22:29 +0100

    Add carta presentacion

--- ClashAPP/src/app/components/index/index.component.ts
+++ ClashAPP/src/app/components/index/index.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { LeaderboardComponent } from '../leaderboard/leaderboard.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-index',
-  imports: [CarouselComponent, LeaderboardComponent],
+  imports: [CarouselComponent, LeaderboardComponent, MatButtonModule, MatCardModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
