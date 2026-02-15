5ea7563194e988c453a85b4c89dd0e21cb54ef7a
egh13 <ericgarciahendricks@gmail.com>
2026-02-15 18:59:04 +0100

    Update index component buttons

--- ClashAPP/src/app/components/index/index.component.ts
+++ ClashAPP/src/app/components/index/index.component.ts
@@ -1,12 +1,13 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { LeaderboardComponent } from '../leaderboard/leaderboard.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [CarouselComponent, LeaderboardComponent, MatButtonModule, MatCardModule],
+  imports: [CarouselComponent, LeaderboardComponent, MatButtonModule, MatCardModule, RouterLink],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
